0cfa09feb36239122717f5bdb78cfbe894c05946
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-21 18:03:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
 function App() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
-
   return (
-    <div className="container">
-      <h1>Welcome to My App</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" name="username" />
-      <button onClick={handleClick}>Click Me</button>
-      <img src="logo.png" alt="Logo" />
+    <div className="App">
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
     </div>
   );
 }
